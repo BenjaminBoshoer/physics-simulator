1a23e8f0aa0135462984ce36f33899663ea58e6a
Yuval Disatnik <yuvaldisatnik28@gmail.com>
2022-03-01 15:27:48 +0200

    added time clock in main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from exec import *
+import time
 
 
 WIN_HEIGHT, WIN_WIDTH = 500, 700
@@ -10,8 +11,12 @@ def main():
     floor = floor_obj(WIN_WIDTH, WIN_HEIGHT * 0.1, (0, 0, 0), WIN_HEIGHT - WIN_HEIGHT*0.1)
 
     print(f"the mass is {obj1.mass}")
-    print("check")
-
+    # currently, the values looks funny.
+    # but it will sort itself once the pygame window ajd the 60 FPS will be implied
+    start_time = time.time()
+    while True:
+        current_time = time.time()
+        print("time: %f" % current_time)
 
 
 if __name__ == "__main__":
