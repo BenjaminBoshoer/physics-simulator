af24aa4dfe63478b18361baca460b3e2a2ce15a8
Beny <boshoer.beny@gmail.com>
2022-03-01 20:04:23 +0200

    Added x_axis_forces and y_axis_forces lists to hold the forces on the objects

--- exec.py
+++ exec.py
@@ -5,6 +5,25 @@ class object:
         self.acc = self.acc_x = self.acc_y = 0
         self.vel = self.vel_x = self.vel_y = 0
         self.location = self.startingLocation = [0,0]
+        self.x_axis_forces = list()
+        self.y_axis_forces = list()
+        self.y_axis_forces.append("-mg")
+
+
+    def add_force(self, variables, axis):
+        if(axis == 'x' or axis == 'X'):
+            x_axis_forces.append(variables)
+
+        if(axis == 'y' or axis == 'Y'):
+            y_axis_forces.append(variables)
+
+
+    def get_forces(self, axis):
+        if(axis == 'x' or axis == 'X'):
+            return x_axis_forces
+        else:
+            return y_axis_forces
+
 
 
 class floor_obj(object):
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from exec import *
 import time
 
-
+G_constant = 9.8
 WIN_HEIGHT, WIN_WIDTH = 500, 700
 
 
@@ -10,13 +10,17 @@ def main():
     obj1 = object(12)
     floor = floor_obj(WIN_WIDTH, WIN_HEIGHT * 0.1, (0, 0, 0), WIN_HEIGHT - WIN_HEIGHT*0.1)
 
-    print(f"the mass is {obj1.mass}")
+    print(f"the mass is {obj1.mass}, and the forces on this object are: ")
+    for i in range(0, len(obj1.y_axis_forces)):
+        print(obj1.y_axis_forces[i])
+
+
     # currently, the values looks funny.
     # but it will sort itself once the pygame window ajd the 60 FPS will be implied
     start_time = time.time()
-    while True:
+    """while True:
         current_time = time.time()
-        print("time: %f" % current_time)
+        print("time: %f" % current_time)"""
 
 
 if __name__ == "__main__":
