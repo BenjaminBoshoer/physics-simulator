d13dd54efde98df20cbe55013a2323ef0620724e
Yuval Disatnik <yuvaldisatnik28@gmail.com>
2022-03-27 11:28:16 +0300

    PEP-8 modifications

--- exec.py
+++ exec.py
@@ -1,76 +1,66 @@
-
 import time
 
-class object:
+
+class Object:
     def __init__(self, weight=0):
         self.mass = weight
         self.acc = self.acc_x = self.acc_y = 0
         self.vel = self.vel_x = self.vel_y = 0
-        self.location = self.startingLocation = [0,0]
+        self.location = self.startingLocation = [0, 0]
         self.x_axis_forces = list()
         self.y_axis_forces = list()
         self.y_axis_forces.append("-mg")
         self.movement_start_time = 0
         self.is_in_motion = 0
 
-
     def add_force(self, variables, axis):
-        if(axis == 'x' or axis == 'X'):
-            x_axis_forces.append(variables)
+        if axis == 'x' or axis == 'X':
+            self.x_axis_forces.append(variables)
 
-        if(axis == 'y' or axis == 'Y'):
-            y_axis_forces.append(variables)
-
+        if axis == 'y' or axis == 'Y':
+            self.y_axis_forces.append(variables)
 
     def get_forces(self, axis):
-        if(axis == 'x' or axis == 'X'):
-            return x_axis_forces
+        if axis == 'x' or axis == 'X':
+            return self.x_axis_forces
         else:
-            return y_axis_forces
-
+            return self.y_axis_forces
 
     def start_moving(self):
         self.movement_start_time = time.time()
         self.is_in_motion = 1
 
-
     def stop_moving(self):
         self.is_in_motion = 0
 
-
-    def get_acc():
-        if(self.is_in_motion == 1):
+    def get_acc(self):
+        if self.is_in_motion == 1:
             pass
-
-
-
 
         else:
             return 0
 
-    # This function retruns the equation for a given axis. Example "F - mg = ma"
+    # This function returns the equation for a given axis. Example "F - mg = ma"
     def get_equation(self, axis):
-
-        if(axis == 'x'):
+        if axis == 'x':
             left = "+".join(self.x_axis_forces)
 
-        elif(axis == 'y'):
+        elif axis == 'y':
             left = "+".join(self.y_axis_forces)
 
-        indx = left.find("+-")
-        if(indx != -1):
-            left = left[0:indx] + left[indx+1:]
+        index = left.find("+-")
+        if index != -1:
+            left = left[0:index] + left[index + 1:]
 
-        if(self.is_in_motion == 1):
+        if self.is_in_motion == 1:
             return left + f" = {self.mass}a"
 
         else:
             return left + f" = 0"
 
 
-
-class floor_obj(object):
-    def __init__(self, width, height, color=(0,0,0), pos_y = 0):
+class FloorObj(object):
+    def __init__(self, width, height, color=(0, 0, 0), pos_y=0):
         self.width = width
         self.height = height
         self.color = color
--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from exec import *
 
-
 G_constant = 9.8
 WIN_HEIGHT, WIN_WIDTH = 500, 700
 
 
 def main():
-
     obj_arr = list()
-    obj1 = object("m")
-    floor = floor_obj(WIN_WIDTH, WIN_HEIGHT * 0.1, (0, 0, 0), WIN_HEIGHT - WIN_HEIGHT*0.1)
+    obj1 = Object("m")
+    floor = FloorObj(WIN_WIDTH, WIN_HEIGHT * 0.1, (0, 0, 0), WIN_HEIGHT - WIN_HEIGHT * 0.1)
 
     obj_arr.append(obj1)
     obj1.start_moving()
 
     print(f"the mass is {obj1.mass}, and the forces on this object are: ")
     print(f"And it's equation is {obj1.get_equation('y')}")
-
 
     # currently, the values looks funny.
     # but it will sort itself once the pygame window ajd the 60 FPS will be implied
@@ -27,7 +24,6 @@ def main():
 
 
 def update_location(current_time, obj_arr):
-
     for obj in obj_arr:
         acc = obj.get_acc()
 
